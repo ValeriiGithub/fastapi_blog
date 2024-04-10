c807fd5cc4ef19ec009b88cadbcb29fd91f48b1f
Valerii <valeri.github@gmail.com>
2024-04-10 17:05:57 +0300

    Bag исправлен

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,6 @@ config = context.config
 
 config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
 
-target_metadata = Base.metadata
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -25,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
