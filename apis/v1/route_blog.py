b797a812c31d5d2dc6ff6f0fde65b0c4936fe376
Valerii <137908068+ValeriiGithub@users.noreply.github.com>
2024-07-29 16:55:11 +0300

    Update route_blog.py

--- apis/v1/route_blog.py
+++ apis/v1/route_blog.py
@@ -9,6 +9,7 @@ from db.repository.blog import create_new_blog, retreive_blog, list_blogs, updat
 
 router = APIRouter()
 
+
 @router.post("/blogs", response_model=ShowBlog, status_code=status.HTTP_201_CREATED)
 async def create_blog(blog: CreateBlog, db: Session = Depends(get_db)):
     blog = create_new_blog(blog=blog, db=db, author_id=1)
